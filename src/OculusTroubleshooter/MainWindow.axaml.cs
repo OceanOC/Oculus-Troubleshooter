e2f3509030c7c69ae10ed2693b2ab084f8bbeb29
OceanOC <141111946+OceanOC@users.noreply.github.com>
2023-10-25 12:14:43 +0530

    New UI

---
+++ src/OculusTroubleshooter/MainWindow.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+using System;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace OculusTroubleshooter
+{
+    public partial class MainWindow : Window
+    {
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
